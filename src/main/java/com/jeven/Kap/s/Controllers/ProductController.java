620ccc165088eb3e14eb886e2cc65ffef3bfba82
jeven2003 <21-10161@g.batstate-u.edu.ph>
2024-06-26 16:39:44 +0800

    Final Activity1

--- src/main/java/com/jeven/Kap/s/Controllers/ProductController.java
+++ src/main/java/com/jeven/Kap/s/Controllers/ProductController.java
@@ -2,12 +2,16 @@ package com.jeven.Kap.s.Controllers;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jeven.Kap.s.Model.Product;
+import com.jeven.Kap.s.NotFoundException.ProductNotFoundException;
 import com.jeven.Kap.s.Repository.ProductRepository;
 
 @RestController
@@ -23,15 +27,35 @@ public class ProductController {
     public List<Product> getProducts (){
         return repo.findAll();
     }
-
-    /* public Product getProduct(Long id){
-        return repo.findById(id);
-    }*/
+    @GetMapping("/product/{id}")
+     public Product getProduct(@PathVariable Long id){
+        return repo.findById(id)
+        .orElseThrow(()-> new ProductNotFoundException(id));
+    }
 
     @PostMapping("/product/new")
     public String addProduct(@RequestBody Product newProduct){
         repo.save(newProduct);
         return "A new product is added, Yehey!";
+    }
+    //UPDATE ENDPOINTS
+    @PutMapping("/product/edit/{id}")
+    public Product updateProduct(@PathVariable Long id, @RequestBody Product newProduct){
+        return repo.findById(id)
+        .map(product->{
+            product.setProductName(newProduct.getProductName());
+            product.setDescription(newProduct.getDescription());
+            product.setPrice(newProduct.getPrice());
+            return repo.save(product);
+        }).orElseGet(()->{
+            return repo.save(newProduct);
+        });
+    }
+    @DeleteMapping ("/product/delete/{id}")
+    public String deleteProduct(@PathVariable Long id){
+        repo.deleteById(id);
+        return "A product is deleted";
+
     }
 
 }
